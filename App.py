dc3066c707f717b917fd4cdfaffbea0751f7f823
Albert-PG <p.plta.albert@gmail.com>
2020-06-14 12:57:34 -0500

    Creation of Query and show data using jinja

--- App.py
+++ App.py
@@ -1,27 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+#MySQL connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '-'
-app.config['MYSQL_DB'] = 'contacts'
+app.config['MYSQL_DB'] = 'test_python'
 
 mysql = MySQL(app)
+
+#Setting
+app.secret_key = 'mysecretkey'
 
 
 #Routes
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    print(data)
 
-@app.route('/add_contact', method = ['POST'])
+    return render_template('index.html', contacts = data)
+
+@app.route('/add_contact', methods = ['POST'])
 def add_contact():
-    if request.method
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)', (fullname, phone, email))
+        mysql.connection.commit()
+        flash('Contact added successfully')
+
+        print(fullname)
+        print(phone)
+        print(email)
+        return redirect(url_for('Index'))
+
 
 @app.route('/edit')
-def edit:
+def edit():
     return 'edit'
 
 @app.route('/delete')
